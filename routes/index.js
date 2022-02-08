b9bba2b13db16e23b1cde7f073844bf59f2580c9
Moonlization <seva2409@gmail.com>
2022-02-08 15:13:44 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+
+
 export default function routes(app, addon) {
     // Redirect root path to /atlassian-connect.json,
     // which will be served by atlassian-connect-express.
@@ -42,6 +44,24 @@ export default function routes(app, addon) {
         //, browserOnly: true // you can set this to disable server-side rendering for react views
       }
     );
+});
+app.get('/installed', addon.authenticate(), (req, res) => {
+  // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
+  // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
+  res.status(200).json(JSON.parse({
+    "key": "installed-addon-key",
+    "clientKey": "unique-client-identifier",
+    "sharedSecret": "a-secret-key-not-to-be-lost",
+    "serverVersion": "server-version",
+    "pluginsVersion": "version-of-connect",
+    "baseUrl": "https://moonlization.atlassian.net",
+    "displayUrl": "https://issues.example.com",
+    "displayUrlServicedeskHelpCenter": "https://support.example.com",
+    "productType": "jira",
+    "description": "Atlassian Jira at https://moonlization.atlassian.net",
+    "serviceEntitlementNumber": "SEN-number",
+    "eventType": "installed"
+  }))
 });
 
     // Add additional route handlers here...
